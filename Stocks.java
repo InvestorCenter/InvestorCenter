644baf0d77415ffebdca8a5fc5354a4ef3df29e2
NickCecc <nickcecchin36@gmail.com>
2024-04-05 11:17:11 -0400

    Implement portfolio determination based on profiles
    Added a new function, 'DeterminePortfolio', in Survey.java to assign portfolios based on user profiles. The function evaluates the risk level, time period, and contribution frequency from profile and returns a switch statement. The changes in Stocks.java are primarily formatting adjustments to the comment layouts.

--- Stocks.java
+++ Stocks.java
@@ -1,31 +1,28 @@
-public class Stocks {
+// Low risk
 
-    // Low risk
+// 1 - 5 years without contributions
 
-    // 1 - 5 years without contributions
-
-    // TD Advantage Balanced Growth Port - F
-    // TD Advantage Balanced Income Port - F
-    // TD Advantage Balanced Growth Port - A
+// TD Advantage Balanced Growth Port - F
+// TD Advantage Balanced Income Port - F
+// TD Advantage Balanced Growth Port - A
 
 
-    // 1 - 5 years with contributions
+// 1 - 5 years with contributions
 
-    // Betterment
-    // EquityMultiple
-    // VanGuard
-
-
-    // 5 - 10 years without contributions
-
-    // TD Advantage Balanced Growth Port - I
-    // TD Advantage Balanced Income Port - I
-    // TD Advantage Balanced Income Port - A
+// Betterment
+// EquityMultiple
+// VanGuard
 
 
-    // 5 - 10 years with contributions
+// 5 - 10 years without contributions
 
-    // S&P 500
-    // SPXL
-    // SPXS
-}
+// TD Advantage Balanced Growth Port - I
+// TD Advantage Balanced Income Port - I
+// TD Advantage Balanced Income Port - A
+
+
+// 5 - 10 years with contributions
+
+// S&P 500
+// SPXL
+// SPXS
